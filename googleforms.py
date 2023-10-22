67e778c1aed2451d489b237c8f1c46a7461a2deb
Kevin Nembhard <nembhardkm2003@gmail.com>
2023-10-22 03:35:26 -0400

    slifght

--- googleforms.py
+++ googleforms.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 import os.path
-import json
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -16,68 +15,84 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # SPREADSHEET_ID: Obtained with the last string of characters in the URL of the spreadsheet that's created
 # SAMPLE_RANGE_NAME: Is the range inside that row of inputs
 SPREADSHEET_ID = '1FxU51lyWODR-tx4Nn4lWnqPEVSyWNamOStpn4_kd6Fo' 
-SAMPLE_RANGE_NAME = 'B:J'
+SAMPLE_RANGE_NAME = 'B2:J4'
+
 user_dict = {}
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
+"""Shows basic usage of the Sheets API.
+Prints values from a sample spreadsheet.
+"""
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+# Login to google to fill out form
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+try:
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
     
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        
-    # If there are no (valid) credentials available, let the user log in.
-    # Login to google to fill out form
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
 
-    try:
-        service = build('sheets', 'v4', credentials=creds)
+    values = result.get('values', [])
+    for int in range(len(values)):
+        print(values[int])
+    
+    if not values:
+        print('No data found.')
+        exit()
+    
+    email = input("What was the email used to fill out the form? ")
+    
+    # # creates a mxn array to store this information (values x row)
+    index = 0
 
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-        
-        # If there are no responses in the Google Sheets
-        if not values:
-            print('No data found.')
-            return
-        
-        email = input("What was the email used to fill out the form? ")
-        
-        # creates a mxn array to store this information (values x row)
-        for row in values: 
-            if row[0] == email:
-                tempRow = row
+    for subRow in values:
+        for row in subRow:
+            if row == email:
+                # Print columns B and I, which correspond to indices 0 and 7.
+                #print('%s %s, %s, %s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])) 
                 
-        # Store the user's input into a dictionary for the Fitness API to read 
-        user_dict = {"email": tempRow[0], "name": tempRow[1], "sex": tempRow[2], "age": tempRow[3], "weight": tempRow[4], "height": tempRow[5], "goals": tempRow[6], "strength": tempRow[7], "muscles": tempRow[8]}
-        print(user_dict)
+                print('\n%s' % (values[index]))
+            else: 
+                index += 1
+    print(str(len(values)) + " is the length of values\n")
     
-    except HttpError as err:
-        print(err)
+    # creates a mxn array to store this information (values x row)
+    for row in values: 
+        if row[0] == email:
+            tempRow = row
     
-    return user_dict
+            
+    print(str(len(tempRow)) + " is the length of tempRow\n")
+    
+    # Store the user's input into a dictionary for the Fitness API to read 
+    keys = ["email", "name", "sex", "age", "weight", "height", "goal", "strength", "muscles"]
 
+    # Ensure tempRow has enough elements
+    if len(tempRow) >= len(keys):
+        user_dict = dict(zip(keys, tempRow))
+    else:
+        print("Error: tempRow does not have enough elements")      
 
-        
-if __name__ == '__main__':
-    main()
-    print("\n" + "Heres the dict")
+except HttpError as err:
+    print(err)
 
-    print(user_dict)
+return user_dict
+
